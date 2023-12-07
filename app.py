a4a8f2297d99eb60e9db92c2546e93565fe1e8e6
Zarina Mazitova <zarina.mazitova@gmail.com>
2023-12-07 15:24:10 +0000

    sanitised API input

--- app.py
+++ app.py
@@ -74,7 +74,8 @@ def before_request():
 
                     try:
                         app_name = response_dict["results"][0]["trackName"]
-                        cursor.execute("""UPDATE app_name SET APPNAME = ? WHERE BUNDLEID = ? AND APPNAME IS NULL""", (app_name, row[0]))
+                        clean_name = app_name.translate(str.maketrans('', '', string.punctuation))
+                        cursor.execute("""UPDATE app_name SET APPNAME = ? WHERE BUNDLEID = ? AND APPNAME IS NULL""", (clean_name, row[0]))
                     except:
                         print("N/A")
 
@@ -82,8 +83,8 @@ def before_request():
                     print("error")
 
             # Remove special characters from the app name
-            clean_results = [i[0].translate(str.maketrans('', '', string.punctuation)) for i in results]
-            apps.append(clean_results)
+            # clean_results = [i[0].translate(str.maketrans('', '', string.punctuation)) for i in results]
+            apps.append(clean_name)
 
         # Add up total screen time for the day
         for row in rows:
