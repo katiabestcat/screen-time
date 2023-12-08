80e94ef5de3bde98cdaec63e2234659e279ecd25
Zarina Mazitova <zarina.mazitova@gmail.com>
2023-12-08 18:15:21 +0000

    corrected typo in comment to port 5000

--- run_app.zsh
+++ run_app.zsh
@@ -5,7 +5,7 @@ cd <path/to/your/project/folder>
 # runs the main app and pings the local host to generate the static html file locally
 python3 -m flask run & sleep 3 && curl http://127.0.0.1:5000
 
-# kills all the processes running on port 500, so that you can run the html generation again later
+# kills all the processes running on port 5000, so that you can run the html generation again later
 kill -9 $(lsof -t -i:"5000")
 
 # runs surge to publish your html page into production at the domain name of your choice
